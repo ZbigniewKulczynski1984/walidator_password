7d064d5366fc6266cf15aa865fbaf89a28c65acb
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-07-27 10:33:43 +0200

    main.js modified

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const showMsg = () => {
+    if (pass.value.length >= minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
+    p.textContent = 'Masz bardzo dobre hasło!!! 💪'
+    p.style.color = 'lime'
+}   else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
+    p.textContent = 'Masz dobre hasło!'
+    p.style.color = 'gold'
+}   else {
+    p.textContent = 'Masz słabe hasło!'
+    p.style.color = 'tomato'
+}
+}
+
+const checkPassword = () => {
+    if (pass.value !==''){
+        showMsg()
+    } else {
+        p.textContent = 'Padaj hasło!'
+        p.style.color = ''
+    }
+}
+
+
+pass.addEventListener('keyup', checkPassword)
+
+
+
+
+// /^[0-9a-z_.-]+@[0-9a-z_.-]+\.[a-z]{2,3}$/i
